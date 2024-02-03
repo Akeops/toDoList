dc55d6ea552b71e6d9cb21d28418777a5399a08f
Ando_lalanne <lalanne.andoni1@gmail.com>
2024-02-03 18:07:18 +0100

    J'affiche toutes les tâches dans des li à partir de mon objet grâce à la méthode

--- app.js
+++ app.js
@@ -1,70 +1,54 @@
-const tacheObjet = {
-     taches: [
-          { id: 0, nameTache: "Faire les courses", date: "" },
-          { id: 1, nameTache: "Travailler 1 heure", date: "" },
-          { id: 2, nameTache: "Faire du sport", date: "" },
-          { id: 3, nameTache: "Marcher", date: "" },
-     ],
-     get mesTaches() {
-          return tacheObjet;
-     },
-
-     uneTache(id, name, date) {
-          tacheObjet.taches.push({
-               id,
-               name,
-               date
-          })
-     }	
-}   
-     
-console.log(tacheObjet.taches);
-tacheObjet.uneTache(4, "Retourner à Bordeaux", "04/02/2024");
-console.log(tacheObjet.taches);
+// Tout le ciblage sur le DOM
 
 const div1 = document.querySelector("#div1");
 const div2 = document.querySelector("#div2");
 const tache = document.querySelector("#tache");
 const buttonAjouter = document.querySelector("#buttonAjouter");
 const ul = document.querySelector("#maListe");
-const buttonSupprimer = document.querySelector(".suppr");
 
-function ajouterUneTache() {
-	const divElementTache = document.createElement("div");
-	const li = document.createElement("li");
-	const buttonSupprimer = document.createElement("button");
-	let isTache = tache.value;
-	if (isTache) {
-		li.textContent = tache.value;
-	} else {
-		li.textContent = "Pas de tache";
-	}
-	divElementTache.className = "divTache";
-	li.className = "liTache";
-	ul.append(divElementTache);
-	divElementTache.append(li);
-	buttonSupprimer.textContent = "Supprimer";
-	buttonSupprimer.className = "suppr";
-     li.appendChild(buttonSupprimer);
-     buttonSupprimer.addEventListener("click", () => {
-           divElementTache.remove();
-     });
+
+// Les objets
+
+const tacheObjet = {
+	taches: [
+		{ importance: 1, nameTache: "Faire les courses", date: "" },
+		{ importance: 1, nameTache: "Travailler 1 heure", date: "" },
+		{ importance: 2, nameTache: "Faire du sport", date: "" },
+		{ importance: 3, nameTache: "Marcher", date: "" },
+	],
+	get mesTaches() {
+		return tacheObjet.taches;
+	},
+	ajoutTache(importance, name, date) {
+		tacheObjet.taches.push({
+			importance,
+			name,
+			date,
+		});
+	},
+};
+
+// Affichage de toutes les tâches 
+
+let resultatMethodeMesTaches = tacheObjet.mesTaches;
+for (let i = 0; i < resultatMethodeMesTaches.length; i++) {
+     const li = document.createElement("li");
+     const pImportance = document.createElement("p");
+     const pName = document.createElement("p");
+     const pDate = document.createElement("p");
+     const buttonSupprimer = document.createElement("button")
+     buttonSupprimer.textContent = "Supprimer";
+     buttonSupprimer.className = "suppr";
+     pImportance.textContent = resultatMethodeMesTaches[i]["importance"];
+     pName.textContent = resultatMethodeMesTaches[i]["nameTache"];
+     pDate.textContent = resultatMethodeMesTaches[i]["date"];
+     ul.append(li);
+     li.append(pImportance);
+     li.append(pName);
+     li.append(pDate);
+     li.append(buttonSupprimer);
 }
+
 
 buttonAjouter.addEventListener("click", ajouterUneTache);
 
-
-// cette fonction ne fonctionne pas
-function suprimerTache() {
-
-	const divElementTache = document.querySelector("divTache");
-     buttonSupprimer.addEventListener("click", () => {
-           divElementTache.remove();
-      });
-	
-}
-
-function toDoList() {
-	ajouterUneTache();
-	suprimerTache();
-}
