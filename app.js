2c329b08032b74b11c2722eede5f7a10a3181ad8
Ando_lalanne <lalanne.andoni1@gmail.com>
2024-02-09 18:36:24 +0100

    J'ai ajouté la fonctionnalite archiver mais je dois l'ajouter visuellement

--- app.js
+++ app.js
@@ -4,63 +4,87 @@ const tache = document.querySelector("#tache");
 const buttonAjouter = document.querySelector("#buttonAjouter");
 const ul = document.querySelector("#maListe");
 
-
-
 class TacheManager {
-  constructor(taches) {
+  constructor(taches, tachesArchivees) {
     this._taches = [];
+    this._tachesArchivees = [];
+  }
+  
+
+	get taches() {
+		return this._taches;
   }
 
-  get taches() {
-    return this._taches;
+  get tachesArchivees() {
+    return this._tachesArchivees;
   }
-
-  ajoutTache(importance, nomTache, date) {
-    const tache = {importance, nomTache, date}
-    this.taches.push(tache);
-  }
-
-  supprimerTache(key) {
+  
+  archiverTache(key) {
     if (key >= 0 && key < this.taches.length) {
-      this.taches.splice(key, 1);
-      console.log("Tâche supprimée avec succès.");
-    } else {
-      console.error("Indice invalide. La tâche n'a pas été supprimée.");
+      
+      const tacheArchivees = this.taches.splice(key, 1)[0];
+      this.tachesArchivees.push(tacheArchivees);
+      
+		  console.log("Tâche archivee avec succès.");
+	  } else {
+		  console.error("Indice invalide. La tâche n'a pas été supprimée.");
     }
   }
+
+	ajoutTache(importance, nomTache, date) {
+		const tache = { importance, nomTache, date };
+		this.taches.push(tache);
+	}
+
+	supprimerTache(key) {
+		if (key >= 0 && key < this.taches.length) {
+			this.taches.splice(key, 1);
+			console.log("Tâche supprimée avec succès.");
+		} else {
+			console.error("Indice invalide. La tâche n'a pas été supprimée.");
+		}
+	}
 }
 
 function getDate() {
-  const date = new Date();
-  const day = date.getDay();
-  const month = date.getMonth();
-  const year = date.getFullYear();
-  try {
-    if (day < 10 || month < 10) {
-      const fullYear = `0${day}/0${month}/${year}`;
-      return fullYear;
-    } else if (day < 10) {
-      const fullYear = `0${day}/${month}/${year}`;
-      return fullYear;
-    } else if (month > 9) {
-      const fullYear = `${day}/0${month}/${year}`;
-      return fullYear;
-    }
-  } catch (e) {
-    return e;
-  }
+	const date = new Date();
+	const day = date.getDay();
+	const month = date.getMonth();
+	const year = date.getFullYear();
+	try {
+		if (day < 10 || month < 10) {
+			const fullYear = `0${day}/0${month}/${year}`;
+			return fullYear;
+		} else if (day < 10) {
+			const fullYear = `0${day}/${month}/${year}`;
+			return fullYear;
+		} else if (month > 9) {
+			const fullYear = `${day}/0${month}/${year}`;
+			return fullYear;
+		}
+	} catch (e) {
+		return e;
+	}
 }
 
-
+// Ajouter une tâche
+document.querySelector("#monForm").addEventListener("submit", (event) => {
+	event.preventDefault();
+	const valeurImportance = document.querySelector("#importance").value;
+	const valeurNomTache = document.querySelector("#tache").value;
+	const date = getDate();
+	tacheManager.ajoutTache(valeurImportance, valeurNomTache, date);
+	afficherTaches();
+  document.querySelector("#importance").value = "";
+  document.querySelector("#tache").value = "";
+});
 
 const tacheManager = new TacheManager();
-// tacheManager.ajoutTache(5, "Aller bosser", getDate());
-// tacheManager.ajoutTache(4, "Faire du sport pendant 1h", getDate());
-// tacheManager.ajoutTache(1, "Marcher 10 000 pas", getDate());
-// tacheManager.ajoutTache(2, "Prendre la vitamine D", getDate());
 
 function afficherTaches() {
-  tacheManager.taches.forEach((tache) => {
+  ul.innerHTML = ""; // Effacer la liste pour éviter les doublons
+
+  tacheManager.taches.forEach((tache, index) => {
     const li = document.createElement("li");
     const buttonSupprimer = document.createElement("button");
     const buttonArchiver = document.createElement("button");
@@ -70,26 +94,33 @@ function afficherTaches() {
     buttonArchiver.textContent = "Archiver";
     buttonArchiver.className = "archiver";
     const tacheDetail = `${tache.importance}  ${tache.nomTache}  ${tache.date}`;
-    console.log(tacheDetail);
-	  li.innerHTML = tacheDetail;
+    console.log(tacheManager.taches);
+    li.textContent = tacheDetail;
     ul.append(li);
     li.append(buttonSupprimer);
     li.append(buttonArchiver);
-
-    buttonAjouter.addEventListener("click", () => {
-      const valeurImportance = document.querySelector("#importance").value;
-      const valeurNomTache = document.querySelector("#tache").value;
-      tacheManager.ajoutTache(valeurImportance, valeurNomTache, getDate());
-    });
 
     buttonSupprimer.addEventListener("click", () => {
       const index = tacheManager.taches.indexOf(tache);
       if (index !== -1) {
         tacheManager.supprimerTache(index);
-        li.remove(); 
+        li.remove();
       }
     });
-  });
+    
+    buttonArchiver.addEventListener("click", () => {
+      tacheManager.archiverTache(index);
+    });
+   
+  })
 }
-console.log(tacheManager.taches)
+
+function afficheTachesArchivves() {
+  tacheManager.tachesArchivees.forEach(tache => {
+    
+  })
+}
+
+
 afficherTaches();
+tacheManager.tachesArchivees
--- test.js
+++ test.js
@@ -1,39 +1,61 @@
 class TacheManager {
-  constructor(taches) {
-    this._taches = [];
-  }
+	constructor(taches, tachesArchivees) {
+		this._taches = [];
+		this._tachesArchivees = [];
+	}
 
-  get taches() {
-    return this._taches;
-  }
+	get taches() {
+		return this._taches;
+	}
 
-  ajoutTache(importance, nomTache, date) {
-    this.taches.push({
-      importance,
-      nomTache,
-      date,
-    });
-  }
+	get tachesArchivees() {
+		return this._tachesArchivees;
+	}
 
-  supprimerTache(key) {
+	archiverTache(key) {
     if (key >= 0 && key < this.taches.length) {
-      // Il vaut mieux utiliser une fonction native qui supprime directement dans le tableau plutôt que delete
-      this.taches.splice(key, 1);
-      console.log("Tâche supprimée avec succès.");
-    } else {
-      console.error("Indice invalide. La tâche n'a pas été supprimée.");
-    }
-  }
+      
+      this.tachesArchivees.push(key.importance, key.nomTache, key.date);
+      console.log(this.taches.key)
+			this.taches.splice(key, 1);
+
+			console.log("Tâche supprimée avec succès.");
+		} else {
+			console.error("Indice invalide. La tâche n'a pas été supprimée.");
+		}
+	}
+
+	ajoutTache(importance, nomTache, date) {
+		const tache = { importance, nomTache, date };
+		this.taches.push(tache);
+	}
 }
 
-const tacheManager = new TacheManager();
-tacheManager.ajoutTache(5, "Aller bosser", "");
-tacheManager.ajoutTache(4, "Faire du sport pendant 1h", "");
-tacheManager.ajoutTache(1, "Marcher 10 000 pas", "");
-tacheManager.ajoutTache(2, "Prendre la vitamine D", "");
-
-
- console.log(tacheManager.taches);
-for (let key in tacheManager.taches) {
- 
+function getDate() {
+	const date = new Date();
+	const day = date.getDay();
+	const month = date.getMonth();
+	const year = date.getFullYear();
+	try {
+		if (day < 10 || month < 10) {
+			const fullYear = `0${day}/0${month}/${year}`;
+			return fullYear;
+		} else if (day < 10) {
+			const fullYear = `0${day}/${month}/${year}`;
+			return fullYear;
+		} else if (month > 9) {
+			const fullYear = `${day}/0${month}/${year}`;
+			return fullYear;
+		}
+	} catch (e) {
+		return e;
+	}
 }
+
+const tacheManager = new TacheManager()
+const unTest = tacheManager.ajoutTache(1, "Allo", getDate());
+//tacheManager.ajoutTache(1, "Allo", getDate())
+tacheManager.archiverTache(0);
+console.log(tacheManager.taches)
+
+console.log(tacheManager.tachesArchivees);
